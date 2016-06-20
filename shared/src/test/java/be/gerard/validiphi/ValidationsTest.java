f33d6291099972316414d41947769636c653863e
bartgerard <bart.gerard@gmail.com>
2016-06-20 19:21:14 +0200

    Validations

---
+++ shared/src/test/java/be/gerard/validiphi/ValidationsTest.java
@@ -0,0 +1,16 @@
+package be.gerard.validiphi;
+
+/**
+ * ValidationsTest
+ *
+ * @author bartgerard
+ * @version v0.0.1
+ */
+public class ValidationsTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
